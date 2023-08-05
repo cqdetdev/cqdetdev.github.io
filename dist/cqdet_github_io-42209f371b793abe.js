16d99e95852e446a93da8cec43738f09209bc69b
cqdetdisc <cqdetdisc@gmail.com>
2023-08-05 14:51:26 -0400

    forgot this server

--- dist/cqdet_github_io-42209f371b793abe.js
+++ dist/cqdet_github_io-42209f371b793abe.js
@@ -827,7 +827,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cqdet_github_io-9335249517302e17_bg.wasm', import.meta.url);
+        input = new URL('cqdet_github_io-42209f371b793abe_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
